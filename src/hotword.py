46d05b8428ca7c67e87fc37b1449e05c9386aa60
tomarsnap <42405521+tomarsnap@users.noreply.github.com>
2019-07-07 12:58:34 +0300

    new startup

--- src/hotword.py
+++ src/hotword.py
@@ -39,7 +39,6 @@ try:
 except NameError:
     FileNotFoundError = IOError
 
-
 WARNING_NOT_REGISTERED = """
     This device is not registered. This means you will not be able to use
     Device Actions or see your device in Assistant Settings. In order to
@@ -56,6 +55,9 @@ pixel_ring.set_brightness(20)
 pixel_ring.pattern = CustomPattern(pixel_ring.show)
 # pixel_ring.change_pattern("google")
 pixel_ring.wakeup()
+subprocess.Popen(["aplay", "/home/pi/piassistant/src/sample-audio/startup_2.wav"], stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
 
 def process_event(event):
     """Pretty prints events.
@@ -76,7 +78,7 @@ def process_event(event):
     if event.type == EventType.ON_RESPONDING_STARTED:
         pixel_ring.speak()
 
-    if event.type == EventType.ON_RESPONDING_FINISHED or event.type == EventType.ON_CONVERSATION_TURN_TIMEOUT:
+    if event.type == EventType.ON_RESPONDING_FINISHED or event.type == EventType.ON_CONVERSATION_TURN_TIMEOUT or event.type == EventType.ON_NO_RESPONSE:
         pixel_ring.off()
 
     if event.type == EventType.ON_END_OF_UTTERANCE:
@@ -150,17 +152,15 @@ def main():
     # Re-register if "device_model_id" is given by the user and it differs
     # from what we previously registered with.
     should_register = (
-        args.device_model_id and args.device_model_id != device_model_id)
+            args.device_model_id and args.device_model_id != device_model_id)
 
     device_model_id = args.device_model_id or device_model_id
 
     with Assistant(credentials, device_model_id) as assistant:
         events = assistant.start()
-        os.system('mpg321 ../sample-audio/startup_2.mp3 &')
         device_id = assistant.device_id
         print('device_model_id:', device_model_id)
         print('device_id:', device_id + '\n')
-
         # Re-register if "device_id" is different from the last "device_id":
         if should_register or (device_id != last_device_id):
             if args.project_id:
@@ -182,14 +182,14 @@ def main():
             if "update yourself" in str(usrcmd).lower():
                 assistant.stop_conversation()
                 process = subprocess.Popen(["git", "-C", "~/piassistant", "pull"], stdin=subprocess.PIPE,
-                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 stdout = process.communicate()[0]
                 print(stdout)
                 subprocess.call(["sudo", "systemctl", "restart", "piassistant.service"], stdin=subprocess.PIPE,
                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             if "turn" and "on" and "pc" in str(usrcmd).lower():
                 assistant.stop_conversation()
-                subprocess.call(["wakeonlan", "D0-50-99-1B-03-5D"], stdin=subprocess.PIPE,
+                subprocess.call(["wakeonlan", "D0:50:99:1B:03:5D"], stdin=subprocess.PIPE,
                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
